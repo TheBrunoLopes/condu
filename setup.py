b37c4468ac99b91f2f3802dc3548ba6ddf744d5b
Bruno Lopes <brunoleonelopes@gmail.com>
2018-04-12 23:08:04 +0100

    added nots on setup

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0b8',  # Required
+    version='1.0.0b9',  # Required
     # ==============================================================
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -65,10 +65,10 @@ setup(
     author_email='brunoleonelopes@gmail.com',  # Optional
     #  ==============================================================
     # https://packaging.python.org/specifications/core-metadata/#maintainer-optional
-    #maintainer='Netflix OSS',  # Optional
+    #not maintainer='Netflix OSS',  # Optional
     #  ==============================================================
     # https://packaging.python.org/specifications/core-metadata/#maintainer-email-optional
-    #maintainer_email='Netflix OSS',  # Optional
+    #not maintainer_email='Netflix OSS',  # Optional
     #  ==============================================================
     # https://packaging.python.org/specifications/core-metadata/#classifier-multiple-use
     classifiers=[
